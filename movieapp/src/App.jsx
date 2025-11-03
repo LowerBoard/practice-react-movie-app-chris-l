1e268f2ab58ffb6db1e581bbd8793f403323a4d3
cmlorbert <cmlorbert@gmail.com>
2025-11-03 11:38:19 -0600

    done

---
+++ movieapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import MovieSelector from './components/MovieSelector';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <MovieSelector />
+      </div>
+    </>
+  )
+}
+
+export default App;
